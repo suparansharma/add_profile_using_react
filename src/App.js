e1cab33539cc55d9dbd77630207111d9d608f29c
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-17 16:56:42 +0600

    all imformation show

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import { useEffect, useState } from 'react';
+import Company from './components/Company/Company';
+
 
 function App() {
+  const [peoples,setPeople]=useState([]);
+
+    useEffect(()=>{
+      fetch('https://mocki.io/v1/a5e5735f-e946-48d3-aaea-833032dc2633')
+      .then(res=>res.json())
+      .then(data=>setPeople(data))
+      .catch(error=>console.log(error))
+    },[])
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>People loaded:{peoples.length}</h1>
+      
         <Header></Header>
+        
+        <ul>
+        {
+          peoples.map(people=><Company people={people}></Company>)
+        }
+        </ul>
+        
+          
+          
+       
+        
       </header>
     </div>
   );
---
+++ src/components/Company/Company.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const Company = (props) => {
+    console.log(props);
+
+
+ const {id,name,image,email,gender,phonenumber,salary}=props.people;
+  
+    return (
+        <div>
+
+            
+            <h3>the people name is {name}</h3>
+            <p>id:{id}</p>
+            <img src={image} alt="" />
+            <p>email:{email}</p>
+            <p>gender:{gender}</p>
+            <p>phonenumber:{phonenumber}</p>
+            <p>salary:{salary}</p>
+
+            
+        </div>
+    );
+};
+
+export default Company;
