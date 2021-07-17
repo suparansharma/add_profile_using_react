afc17fde1578129089e01f64f7b49dc027d0dd09
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-17 13:51:00 +0600

    header design

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/logo.jpg'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header"> 
+            <h1>Project start from here</h1>
+            <img src={logo} alt="" />
+            <nav>
+                <a href="">All Suggest</a>
+                <a href="">Select People</a>
+                <a href="">ALL</a>
+                </nav>
+        </div>
+    );
+};
+
+export default Header;
